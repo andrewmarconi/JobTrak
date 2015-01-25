22052d703fecdffd6ed627534f85da8425af0d06
Andrew Marconi <andrew@marconimedia.com>
2015-01-25 15:37:44 -0500

    Added some docs to the models.py file. Also see the wiki for more extensive documentation.

--- src/mmg/jobtrak/util/models.py
+++ src/mmg/jobtrak/util/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 import requests
 from bs4 import BeautifulSoup
+
+# ## Sample of Scraping a Site to extract the company name
+# r = requests.get('<<URL>>')
+# soup = BeautifulSoup(r.text)
+# company = soup.find('span', class_='company').string
