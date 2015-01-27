81ef321ebbab5afbd4eab2a422d5777cec0e9be4
Andrew Marconi <andrew@marconimedia.com>
2015-01-27 02:00:40 -0500

    This is related to issue #101. Functionality is stashed for issue #102 until bug can be resolved.

--- src/JobTrak/urls.py
+++ src/JobTrak/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls import include, url, i18n
+#from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-urlpatterns = i18n_patterns('',
+urlpatterns = i18n.i18n_patterns('',
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^app/', include('mmg.jobtrak.core.urls')),
