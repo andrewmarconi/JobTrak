fbe2ab35213c10fb6105b2fc92270fd538e70b2b
Andrew Marconi <andrew@559labs.com>
2016-07-19 13:12:40 -0500

    A little more cleanup with the URL structure.

--- web/code/JobTrak/urls.py
+++ web/code/JobTrak/urls.py
@@ -3,19 +3,15 @@ from django.conf.urls import include, url, i18n
 from django.contrib import admin
 from JobTrak.admin import JobTrakAdmin
 
-urlpatterns = i18n.i18n_patterns(
-
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-
-#    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-#    url(r'^admin/', include(admin.site.urls)),
-
+urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#    url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', include (JobTrakAdmin.urls)),
+]
 
+urlpatterns += i18n.i18n_patterns(
     url(r'^app/contact/', include('mmg.jobtrak.contact.urls', namespace='contact')),
     url(r'^app/', include('mmg.jobtrak.core.urls', namespace='core')),
-
     url(r'^', include('mmg.jobtrak.public.urls')),
-
 )
